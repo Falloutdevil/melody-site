e77b678ddf898fa9448f524abd2ddec20904071a
Falloutdevil <Falloutdevil@ya.ru>
2021-12-13 18:24:46 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     let floorPath =  $('.home-image path'); // каждый отдельный этаж в SVG
     let counterUp = $('.counter-up') // кнопка увелечения этажа
     let counterDown = $('.counter-down') // кнопка уменьшения этажа
+    let modal = $('.modal');
+    let modalCloseButton = $('.modal-close-button');
+    let viewFlatsButton = $('.view-flats');
     
     // функция при наведение мышью на этаж 
     floorPath.on('mouseover', function() {
@@ -10,6 +13,11 @@ $(document).ready(function() {
         currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
         $('.counter').text(currentFloor); // записываем значение этажа в счетчик справа
     });
+
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
+    
     counterUp.on('click', function() { // отслеживаем лик по кнопке вверх
        if (currentFloor < 18) { // провереряем значение этажа, оно не должно быть больше 18
            currentFloor++; // прибваляем один этаж
@@ -27,5 +35,8 @@ $(document).ready(function() {
             floorPath.removeClass('current-floor');
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
-    })
+    });
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }
 });
